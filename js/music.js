d4472cb2cd2f9a2424b005c33936309d2ee0e746
Igoryakib <Igoryakib@gmail.com>
2021-04-25 14:38:18 +0300

    added new files

--- js/music.js
+++ js/music.js
@@ -1,7 +1,7 @@
 const divWrapperRef = document.querySelector('.wrapper__music');
 const musicRef = document.querySelector('.music');
-musicRef.volume = 0.1;
 const volumeRef = document.createElement('img');
+const volumeRangeRef = document.querySelector('#volume_level');
 volumeRef.setAttribute('src', "https://img.icons8.com/ultraviolet/80/000000/no-audio--v2.png");
 volumeRef.setAttribute('alt', 'volume');
 volumeRef.setAttribute('data-index', 1);
@@ -16,12 +16,15 @@ divWrapperRef.append(mutedVolumeRef, volumeRef);
 const onClick = (event) => {
     if(+event.target.dataset.index === 1) {
         musicRef.pause();
+        volumeRangeRef.value = 0;
         event.target.classList.add('close_icon');
         mutedVolumeRef.classList.remove('close_icon');
         return mutedVolumeRef.classList.add('show_icon');
     }
     if (+event.target.dataset.index === 2) {
         musicRef.play();
+        musicRef.volume = 0.5;
+        volumeRangeRef.value = 50;
         event.target.classList.add('close_icon');
         volumeRef.classList.remove('close_icon');
         return volumeRef.classList.add('show_icon');
@@ -33,5 +36,22 @@ const playMusic = () => {
     musicRef.play();
 };
 
+const getRangeVolume = () => {
+    musicRef.volume = volumeRangeRef.value / 100;
+    console.log(musicRef.volume)
+    if (musicRef.volume === 0) {
+        volumeRef.classList.add('close_icon');
+        mutedVolumeRef.classList.remove('close_icon');
+        return mutedVolumeRef.classList.add('show_icon');
+    }
+    if (musicRef.volume >= 0) {
+        mutedVolumeRef.classList.add('close_icon');
+        volumeRef.classList.remove('close_icon');
+        return volumeRef.classList.add('show_icon');
+    }
+
+};
+
 divWrapperRef.addEventListener('click', onClick);
 window.addEventListener('load', playMusic);
+volumeRangeRef.addEventListener('input', getRangeVolume);
