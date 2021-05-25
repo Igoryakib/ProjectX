da5f04d28800deba009c575ec2e469bee4a12344
Igoryakib <Igoryakib@gmail.com>
2021-05-25 20:37:12 +0300

    fix

--- js/game.js
+++ js/game.js
@@ -1,10 +1,54 @@
+// !!!!!!!!!Const Variable!!!!!!!!!
 import arrayCards from "./arrayCards.js";
 
-const wrapperRef = document.querySelector('.wrapper');
+const divWrapperRef = document.querySelector('.wrapper__music');
+
+const musicRef = document.querySelector('.music');
+
+const volumeRef = document.createElement('img');
+
+const volumeRangeRef = document.querySelector('#volume_level');
+
+volumeRef.setAttribute('src', "https://img.icons8.com/ultraviolet/80/000000/no-audio--v2.png");
+
+volumeRef.setAttribute('alt', 'volume');
+
+volumeRef.setAttribute('data-index', 1);
+
+volumeRef.classList.add('background_music');
+
+const mutedVolumeRef = document.createElement('img');
+
+mutedVolumeRef.setAttribute('src', 'https://img.icons8.com/ultraviolet/55/000000/no-audio--v1.png');
+
+mutedVolumeRef.setAttribute('alt', 'muted_volume');
+
+mutedVolumeRef.setAttribute('data-index', 2);
+
+mutedVolumeRef.classList.add('background_music_muted');
+
+divWrapperRef.append(mutedVolumeRef, volumeRef);
+
 const rootRef = document.querySelector(".wrapper__root__wrapper__cards");
+
 const audioEffectRef = document.querySelector(".audio_Effect");
+
 const audioEffectInCorrectRef = document.querySelector(".in_correct");
+
 const correctAudioRef = document.querySelector('.correct_audio');
+
+const minRef = document.querySelector('.mins');
+
+const secRef = document.querySelector('.secs');
+
+const endTimeRef = document.querySelector('.endGame');
+
+const modalEndGameRef = document.querySelector('.modal');
+
+const modalVideoRef = document.querySelector('.video');
+
+const boxButtonsRef = document.querySelector(".btn_box");
+
 let cardRef;
 let doubledCardRef;
 let contentDivRef;
@@ -12,9 +56,21 @@ let doubledContentDivRef;
 let firstCard;
 let secondCard;
 let flipedCard = false;
+let startTime = 10;
 const arrayFlipCards = [];
 arrayCards.length = 6;
 
+// !!!!!!!!!!!!Functions!!!!!!!!!!!!!
+
+// !!!!!!!!!!!!!!Render cards!!!!!!!!!!!!!
+
+// !!!!!!!!!!!Music site!!!!!!!!!!!!!!
+  musicRef.currentTime = 0;
+  musicRef.play();
+  musicRef.volume = 0.28;
+  volumeRangeRef.value = 20;
+
+// !!!!!!!!!!!Music site!!!!!!!!!!!!!!
 
 const RenderCardsFn = (arrayCards) => {
   const result = arrayCards.map((item) => {
@@ -59,9 +115,13 @@ const RenderCardsFn = (arrayCards) => {
     doubledCardRef.style.order = Math.round(Math.random() * arrayCards.length);
     return rootRef.append(cardRef, doubledCardRef);
   });
+ 
+
   return result;
 };
 console.log(RenderCardsFn(arrayCards));
+
+// !!!!!!!!!!!!!!!!Game logic!!!!!!!!!!!!!!!!!!
 
 const openCard = (item) => {
   audioEffectRef.play();
@@ -131,3 +191,96 @@ const inCorrectPairCards = (Card1, Card2) => {
 rootRef.addEventListener("click", onClick);
 console.log(flipedCard);
 console.log('global', arrayFlipCards);
+
+// !!!!!!!!!!!Timer!!!!!!!!!!
+
+const timer = () => {
+  startTime -=1;
+  minRef.textContent = ` 0${Math.floor(startTime / 60)}`;
+  secRef.textContent = `: ${startTime % 60}`;
+
+};
+
+const timerId = setInterval(() => {
+  timer();
+  if (startTime === 60) {
+    minRef.style.color = 'red';
+    secRef.style.color = 'red';
+  }
+  if (startTime === 0) {
+    endTimeRef.play();
+    minRef.textContent = ` 00`;
+    secRef.textContent = `: 00`;
+    clearInterval(timerId);
+    modalEndGameRef.classList.add('show_icon');
+    modalVideoRef.play();
+    const show_btn = setInterval(() => {
+      boxButtonsRef.classList.add("show_icon")
+      clearInterval(show_btn);
+  }, 4000)
+  }
+}, 1000)
+
+// if (startTime === 10) {
+//   minRef.style.color = 'red';
+//   secRef.style.color = 'red';
+// }
+
+// if (startTime === 0) {
+//   endTimeRef.play();
+//   minRef.textContent = ` 00`;
+//   secRef.textContent = `: 00`;
+//   // clearInterval(timerId);
+//   modalEndGameRef.classList.add('show_icon');
+//   console.log(modalEndGameRef)
+//   modalVideoRef.play();
+//   const show_btn = setInterval(() => {
+//     boxButtonsRef.classList.add("show_icon")
+//     clearInterval(show_btn);
+// }, 4000)
+// }
+
+// !!!!!!!!!!!!!!!!Volume icon Function!!!!!!!!!!!!!!!!
+
+const onClickVolumeIcon = (event) => {
+  if(+event.target.dataset.index === 1) {
+      musicRef.pause();
+      volumeRangeRef.value = 0;
+      event.target.classList.add('close_icon');
+      event.target.classList.remove('show_icon');
+      mutedVolumeRef.classList.remove('close_icon');
+      return mutedVolumeRef.classList.add('show_icon');
+  }
+  if (+event.target.dataset.index === 2) {
+      musicRef.play();
+      musicRef.volume = 0.5;
+      volumeRangeRef.value = 50;
+      event.target.classList.add('close_icon');
+      event.target.classList.remove('show_icon');
+      volumeRef.classList.remove('close_icon');
+      return volumeRef.classList.add('show_icon');
+  }
+
+}
+
+const getRangeVolume = () => {
+  musicRef.volume = volumeRangeRef.value / 100;
+  console.log(musicRef.volume)
+  if (musicRef.volume === 0) {
+      volumeRef.classList.add('close_icon');
+      volumeRef.classList.remove('show_icon');
+      mutedVolumeRef.classList.remove('close_icon');
+      return mutedVolumeRef.classList.add('show_icon');
+  }
+  if (musicRef.volume >= 0) {
+      mutedVolumeRef.classList.add('close_icon');
+      mutedVolumeRef.classList.remove('show_icon');
+      volumeRef.classList.remove('close_icon');
+      return volumeRef.classList.add('show_icon');
+  }
+
+};
+
+
+divWrapperRef.addEventListener('click', onClickVolumeIcon);
+volumeRangeRef.addEventListener('input', getRangeVolume);
